b90bf82eed91ceb6edc76a4eb8c2c30def6044fd
Max Ivanov <vonavimax@gmail.com>
2022-05-22 14:09:25 +0700

    Refactoring and add webpack * add webpack dependencies * add build script * add webpack config * fix filenames and imports * add ignore rules

--- index.js
+++
@@ -1,3 +0,0 @@
-export { createApiContainer } from './src/api-container';
-export { useHttpService } from './src/http-service';
-export { BaseService } from './src/BaseService';
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { createApiServiceContainer } from './ioc';
+export { useHttpService } from './http-service';
+export { BaseService } from './BaseService';
