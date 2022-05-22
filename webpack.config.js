802df4fb87c71fa13053deaaf3e3e7c880ac5cae
Max Ivanov <vonavimax@gmail.com>
2022-05-22 14:35:11 +0700

    change build type

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,14 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'module',
+    },
   },
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  experiments: {
+    outputModule: true,
+  },
 };
