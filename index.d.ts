cf8a41e3fd586c9e8e4d2824c0d1ff6b8a408521
Max Ivanov <vonavimax@gmail.com>
2022-06-14 14:03:04 +0700

    update methods contract types

--- index.d.ts
+++ index.d.ts
@@ -3,13 +3,13 @@ import { AxiosInstance, AxiosRequestConfig } from 'axios';
 export class BaseService {
     constructor(httpService: AxiosInstance);
     setHeader(key: string, value: string): void;
-    get(url: string, config: AxiosRequestConfig): Promise<any>;
-    delete(url: string, config: AxiosRequestConfig): Promise<any>;
-    head(url: string, config: AxiosRequestConfig): Promise<any>;
-    options(url: string, config: AxiosRequestConfig): Promise<any>;
-    post(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
-    put(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
-    patch(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
+    get(url: string, config?: AxiosRequestConfig): Promise<any>;
+    delete(url: string, config?: AxiosRequestConfig): Promise<any>;
+    head(url: string, config?: AxiosRequestConfig): Promise<any>;
+    options(url: string, config?: AxiosRequestConfig): Promise<any>;
+    post(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
+    put(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
+    patch(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
     request(config: AxiosRequestConfig): Promise<any>;
     private readonly httpService: AxiosInstance;
 }
@@ -27,13 +27,13 @@ export function useHttpService(
 
 export interface IServiceInstance {
     setHeader(key: string, value: string): void;
-    get(url: string, config: AxiosRequestConfig): Promise<any>;
-    delete(url: string, config: AxiosRequestConfig): Promise<any>;
-    head(url: string, config: AxiosRequestConfig): Promise<any>;
-    options(url: string, config: AxiosRequestConfig): Promise<any>;
-    post(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
-    put(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
-    patch(url: string, payload: unknown, config: AxiosRequestConfig): Promise<any>;
+    get(url: string, config?: AxiosRequestConfig): Promise<any>;
+    delete(url: string, config?: AxiosRequestConfig): Promise<any>;
+    head(url: string, config?: AxiosRequestConfig): Promise<any>;
+    options(url: string, config?: AxiosRequestConfig): Promise<any>;
+    post(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
+    put(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
+    patch(url: string, payload: unknown, config?: AxiosRequestConfig): Promise<any>;
     request(config: AxiosRequestConfig): Promise<any>;
 }
 
